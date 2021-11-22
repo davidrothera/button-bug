68f45552951a6a1687f62f685034b1184a167ac8
David Rothera <david.rothera@gmail.com>
2021-11-22 22:14:33 +0000

    Initial Commit

---
+++ Button Bug/Button_BugApp.swift
@@ -0,0 +1,17 @@
+//
+//  Button_BugApp.swift
+//  Button Bug
+//
+//  Created by David Rothera on 22/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct Button_BugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
